037524473b74d32060283089803a11ccbe47b4da
Andrew Durrant <andrew.durrant75@gmail.com>
2023-03-21 14:02:40 -0600

    Add contact icons to new asset folder. Rip out stylings in home.module.css. Add the four main pages with links.

---
+++ src/pages/experience.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function Experience() {
+  return (
+    <>
+    <h1>Experience </h1>
+    <Link href="/">Home</Link>
+    </>
+  );
+}
---
+++ src/pages/skills-and-projects.tsx
@@ -0,0 +1,10 @@
+import Link from 'next/link';
+
+export default function SkillsAndProjects() {
+  return (
+    <>
+    <h1>Skills & Projects</h1>
+    <Link href="/">Home</Link>
+    </>
+  );
+}
