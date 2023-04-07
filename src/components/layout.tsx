92e88843454fd18ad32f93b37d922bbd9667566f
Andrew Durrant <andrew.durrant75@gmail.com>
2023-04-07 10:23:50 -0600

    Add grid display to layout for better control of card area. Add screenshots for Avybe.Add copy to Airin experience.

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -1,13 +1,16 @@
 import Navigation from './navigation'
 import Title from './title'
+import styles from '@/styles/Home.module.css';
 
 //TODO: Need to type children.
 export default function Layout({ children }: any) {
   return (
     <>
-      <Title/>
-      <Navigation />
-      <main>{children}</main>
+      <div className={styles.siteLayoutContainer}>
+        <Title/>
+        <Navigation />
+        {children}
+      </div>
     </>
   )
 }
--- src/pages/experience.tsx
+++ src/pages/experience.tsx
@@ -11,6 +11,14 @@ import pastSessionsMobile from './/../assets/past-sessions-mobile.jpg';
 import sessionMobile from './/../assets/session-mobile.jpg';
 import modelBuilder from './/../assets/model-builder.jpg';
 import modelLibrary from './/../assets/model-library.jpg';
+import pastSessions from './/../assets/past-sessions.jpg';
+import createTeamOne from './/../assets/create-team-one.jpg';
+import createTeamTwo from './/../assets/create-team-two.jpg';
+import avybeLandingPage from './/../assets/avybe-landing-page.jpg';
+import avybeLandingPageTwo from './/../assets/avybe-landing-page-two.jpg';
+import avybeCollage from './/../assets/avybe-collage.jpg';
+import creatorDashboard from './/../assets/creator-dashboard.jpg';
+
 
 export default function Experience() {
   return (
@@ -24,28 +32,35 @@ export default function Experience() {
     <div className={styles.cardsContainer}>
       <div className={styles.cardDark}>
         <h2>Airin | Cognitive AI</h2>
-        <p>Enterprise SaaS startup using Cognitive AI to build expert systems that reduce human labor costs. Airin has worked with very large clients in both the Tech and BPO spaces including Google, Microsoft, Dealertrack and Accenture. Airin has a very small engineering team where I had the privilege of being one of their first innovators.</p>
+        <p>Enterprise SaaS startup using Cognitive AI to build expert systems that reduce human labor costs. Airin has worked with very large clients in both the Tech and BPO spaces including Google, Microsoft, Dealertrack and Accenture. Airin has a very small engineering team where I had the privilege of being one of their first innovators as a Frontend Engineer. Screenshots are examples of my work at Airin.</p>
         <Image
           className={styles.desktopImages}
           src={myTeams}
           alt='Image of the My Teams view in Airin app'
           width={300}
         />
-        <p>Developed front-end components using TypeScript, Angular, SaSS, HTML and collaborated with back-end engineers to integrate code with server-side logic.</p>
+        <p>Collaborated with product and design to write user stories based off current business priorities and create wireframes for business solutions at an enterprise scale.</p>
         <Image
           className={styles.desktopImages}
           src={myModels}
           alt='Image of the My Models view in Airin app'
           width={300}
         />
-        <p>Code maintenance and bug fixes across all front-end repositories.</p>
+        <p>Collaborated with back-end engineers to integrate front-end code with server-side logic.</p>
         <Image
           className={styles.desktopImages}
           src={modelBuilder}
           alt='Image of the My Teams view in Airin app'
           width={300}
         />
-        <p>Led major updates to the frontend testing infrastructure, including implementing Cypress E2E testing and implementing Gitlab CI/CD pipeline. Worked collaboratively with our manual testers to develop a new testing strategy that improved the overall quality of our product workflow.</p>
+        <p>Led major updates to the frontend testing infrastructure, including implementing Cypress E2E testing and collaborating on the implementation of a Gitlab CI/CD pipeline to further automate testing.</p>
+        <Image
+          className={styles.desktopImages}
+          src={pastSessions}
+          alt='Image of the My Teams view in Airin app'
+          width={300}
+        />
+        <p>Code maintenance and bug fixes across all front-end repositories.</p>
         <Image
           className={styles.desktopImages}
           src={modelLibrary}
@@ -62,6 +77,7 @@ export default function Experience() {
       </div>
       <div className={styles.cardDark}>
         <h2>Airin | Frontend Engineer</h2>
+        <p>Developed front-end architecture and components using TypeScript, Angular, SaSS, HTML based off Figma designs.</p>
         <Image
           className={styles.mobileImages}
           src={myTeamsMobile}
@@ -88,13 +104,59 @@ export default function Experience() {
           width={200}
         />
         <p>Tech Stack: Angular, TypeScript, Firebase, GCP, Docker</p>
+        <Image
+          className={styles.desktopImages}
+          src={createTeamOne}
+          alt='Image of the User Avatar selection modal in Airin app'
+          width={300}
+        />
+        <Image
+          className={styles.desktopImages}
+          src={createTeamTwo}
+          alt='Image of the User Avatar selection modal in Airin app'
+          width={300}
+        />
       </div>
       <div className={styles.cardDark}>
         <h2>Avybe | Software Developer</h2>
+        <p>Reinvented media platform and monetization hub for content creators and entertainers. Avybe allows creators to monetize their fans with the highest willingness to pay through virtual interactions and gamification. Third engineer on a hungry and dedicated team.</p>
+        <Image
+          className={styles.desktopImages}
+          src={avybeLandingPage}
+          alt='Image of the Avybe landing page'
+          width={300}
+        />
         <p>Enhanced functionality and scalability of a video streaming platform catering to content creators.</p>
+        <Image
+          className={styles.desktopImages}
+          src={avybeLandingPageTwo}
+          alt='Image of Avybe landing page part two.'
+          width={300}
+        />
         <p>Developed and expanded the video history functionality for both the client and API.</p>
+        <Image
+          className={styles.desktopImages}
+          src={creatorDashboard}
+          alt='Image of the Avybe creator dashboard'
+          width={300}
+        />
         <p>Designed and implemented the “Follow” Creator functionality for both the mobile app and API, driving engagement and user growth for the product.</p>
+        <div className="video">
+          <iframe 
+          width="560" 
+          height="315" 
+          src="https://www.youtube.com/embed/ZDsTW-cpoRo" title="YouTube video player" 
+          frameborder="0" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          allowfullscreen/>
+        </div>
         <p>Worked with product and design and other developers to implement new designs and functionality to the app and website.</p>
+        <Image
+          className={styles.desktopImages}
+          src={avybeCollage}
+          alt='Collage of Avybe application'
+          width={300}
+        />
         <p>Tech Stack: React, React Native, NextJS, Django, Python, AWS</p>
       </div>
       <div className={styles.cardDark}>
